72cfd0ce3d3a471783869f5ed919d30a1c2367cd
jacobvrosario <84691899+jacobvrosario@users.noreply.github.com>
2026-03-11 00:04:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ st.markdown("""
         margin-bottom: 12px;
     }
     .panel {
-        background: white;
+        background: red;
         padding: 14px 16px;
         border-radius: 14px;
         border: 1px solid #e5e7eb;
